e55f79eafa51f5b8eb10864d5f06e04b8a0c9fad
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-11-08 09:47:43 +0100

    dependcies fixed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+// Replace `createRoot` with `ReactDOM.render`
+ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+  document.getElementById('root')!
+);
